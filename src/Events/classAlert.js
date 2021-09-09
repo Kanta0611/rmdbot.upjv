a5f6168e123af4a9c1be7099ef58285da0ea0dc1
Nero611 <lucasdumont.60@gmail.com>
2021-09-10 00:58:43 +0200

    Formule de calcul du temps de cours trouvée !

--- src/Events/classAlert.js
+++ src/Events/classAlert.js
@@ -1,9 +1,20 @@
 const { MessageEmbed } = require("discord.js");
 const Event = require("../Structures/Event.js");
 
+function msToTime(ms) {
+    let minutes = Math.floor(ms / (1000 * 60));
+    let hours = Math.floor(ms / (1000 * 60 * 60));
+    if (minutes === 0) return `${hours}`;
+    else return `${hours}h${minutes}`;
+  }
+
 module.exports = new Event("classAlert", (client, alert) => {
+    var firstdate = new Date(alert.start);
+    var seconddate = new Date(alert.end);
+    var duration = msToTime(seconddate.getTime() - firstdate.getTime());
+    
     const alertEmbed = new MessageEmbed()
         .setColor(client.config.colors.blue)
         .setTitle(`Cours de ${alert.name} dans 15 minutes !`)
-        .setDescription(`Lieu : ${alert.place}\nProfesseur : ${alert.teacher}\n Durée :`)
+        .setDescription(`Lieu : ${alert.place}\nProfesseur : ${alert.teacher}\n Durée : ${duration}`)
 });
