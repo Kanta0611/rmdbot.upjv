82435c36fbd74cdfe212bed3222d1c662e5ca21a
yama <42041037+oyamabs@users.noreply.github.com>
2021-09-11 15:59:26 +0200

    Ajouts divers (voir desc)
    + ajouts du champ "room" dans le JSON
    + ajout des ID dans le tableau de l'index.php
    + ajout d'un peu de code dans parser.js (detection de l'heure et ajout d'un element dans le tableau)

--- src/Website/index.php
+++ src/Website/index.php
@@ -35,250 +35,249 @@
             </tr>
             <tr>
                 <td>8h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-8">xxxx</td>
+                <td id="tue-8">xxxx</td>
+                <td id="wed-8">xxxx</td>
+                <td id="thu-8">xxxx</td>
+                <td id="fri-8">xxxx</td>
+                <td id="sat-8">xxxx</td>
             </tr>
             <tr>
                 <td>8h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-830">xxxx</td>
+                <td id="tue-830">xxxx</td>
+                <td id="wed-830">xxxx</td>
+                <td id="thu-830">xxxx</td>
+                <td id="fri-830">xxxx</td>
+                <td id="sat-830">xxxx</td>
             </tr>
             <tr>
                 <td>9h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-9">xxxx</td>
+                <td id="tue-9">xxxx</td>
+                <td id="wed-9">xxxx</td>
+                <td id="thu-9">xxxx</td>
+                <td id="fri-9">xxxx</td>
+                <td id="sat-9">xxxx</td>
             </tr>
             <tr>
                 <td>9h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-930">xxxx</td>
+                <td id="tue-930">xxxx</td>
+                <td id="wed-930">xxxx</td>
+                <td id="thu-930">xxxx</td>
+                <td id="fri-930">xxxx</td>
+                <td id="sat-930">xxxx</td>
             </tr>
             <tr>
                 <td>10h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-10">xxxx</td>
+                <td id="tue-10">xxxx</td>
+                <td id="wed-10">xxxx</td>
+                <td id="thu-10">xxxx</td>
+                <td id="fri-10">xxxx</td>
+                <td id="sat-10">xxxx</td>
             </tr>
             <tr>
                 <td>10h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1030">xxxx</td>
+                <td id="tue-1030">xxxx</td>
+                <td id="wed-1030">xxxx</td>
+                <td id="thu-1030">xxxx</td>
+                <td id="fri-1030">xxxx</td>
+                <td id="sat-1030">xxxx</td>
             </tr>
             <tr>
                 <td>11h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-11">xxxx</td>
+                <td id="tue-11">xxxx</td>
+                <td id="wed-11">xxxx</td>
+                <td id="thu-11">xxxx</td>
+                <td id="fri-11">xxxx</td>
+                <td id="sat-11">xxxx</td>
             </tr>
             <tr>
                 <td>11h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1130">xxxx</td>
+                <td id="tue-1130">xxxx</td>
+                <td id="wed-1130">xxxx</td>
+                <td id="thu-1130">xxxx</td>
+                <td id="fri-1130">xxxx</td>
+                <td id="sat-1130">xxxx</td>
             </tr>
             <tr>
                 <td>12h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-12">xxxx</td>
+                <td id="tue-12">xxxx</td>
+                <td id="wed-12">xxxx</td>
+                <td id="thu-12">xxxx</td>
+                <td id="fri-12">xxxx</td>
+                <td id="sat-12">xxxx</td>
             </tr>
             <tr>
                 <td>12h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1230">xxxx</td>
+                <td id="tue-1230">xxxx</td>
+                <td id="wed-1230">xxxx</td>
+                <td id="thu-1230">xxxx</td>
+                <td id="fri-1230">xxxx</td>
+                <td id="sat-1230">xxxx</td>
             </tr>
             <tr>
                 <td>13h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-13">xxxx</td>
+                <td id="tue-13">xxxx</td>
+                <td id="wed-13">xxxx</td>
+                <td id="thu-13">xxxx</td>
+                <td id="fri-13">xxxx</td>
+                <td id="sat-13">xxxx</td>
             </tr>
             <tr>
                 <td>13h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1330">xxxx</td>
+                <td id="tue-1330">xxxx</td>
+                <td id="wed-1330">xxxx</td>
+                <td id="thu-1330">xxxx</td>
+                <td id="fri-1330">xxxx</td>
+                <td id="sat-1330">xxxx</td>
             </tr>
             <tr>
                 <td>14h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-14">xxxx</td>
+                <td id="tue-14">xxxx</td>
+                <td id="wed-14">xxxx</td>
+                <td id="thu-14">xxxx</td>
+                <td id="fri-14">xxxx</td>
+                <td id="sat-14">xxxx</td>
             </tr>
             <tr>
                 <td>14h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1430">xxxx</td>
+                <td id="tue-1430">xxxx</td>
+                <td id="wed-1430">xxxx</td>
+                <td id="thu-1430">xxxx</td>
+                <td id="fri-1430">xxxx</td>
+                <td id="sat-1430">xxxx</td>
             </tr>
             <tr>
                 <td>15h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-15">xxxx</td>
+                <td id="tue-15">xxxx</td>
+                <td id="wed-15">xxxx</td>
+                <td id="thu-15">xxxx</td>
+                <td id="fri-15">xxxx</td>
+                <td id="sat-15">xxxx</td>
             </tr>
             <tr>
                 <td>15h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1530">xxxx</td>
+                <td id="tue-1530">xxxx</td>
+                <td id="wed-1530">xxxx</td>
+                <td id="thu-1530">xxxx</td>
+                <td id="fri-1530">xxxx</td>
+                <td id="sat-1530">xxxx</td>
             </tr>
             
             <tr>
                 <td>16h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-16">xxxx</td>
+                <td id="tue-16">xxxx</td>
+                <td id="wed-16">xxxx</td>
+                <td id="thu-16">xxxx</td>
+                <td id="fri-16">xxxx</td>
+                <td id="sat-16">xxxx</td>
             </tr>
             <tr>
                 <td>16h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1630">xxxx</td>
+                <td id="tue-1630">xxxx</td>
+                <td id="wed-1630">xxxx</td>
+                <td id="thu-1630">xxxx</td>
+                <td id="fri-1630">xxxx</td>
+                <td id="sat-1630">xxxx</td>
             </tr>
             <tr>
                 <td>17h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-17">xxxx</td>
+                <td id="tue-17">xxxx</td>
+                <td id="wed-17">xxxx</td>
+                <td id="thu-17">xxxx</td>
+                <td id="fri-17">xxxx</td>
+                <td id="sat-17">xxxx</td>
             </tr>
             <tr>
                 <td>17h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1730">xxxx</td>
+                <td id="tue-1730">xxxx</td>
+                <td id="wed-1730">xxxx</td>
+                <td id="thu-1730">xxxx</td>
+                <td id="fri-1730">xxxx</td>
+                <td id="sat-1730">xxxx</td>
             </tr>
             <tr>
                 <td>18h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-18">xxxx</td>
+                <td id="tue-18">xxxx</td>
+                <td id="wed-18">xxxx</td>
+                <td id="thu-18">xxxx</td>
+                <td id="fri-18">xxxx</td>
+                <td id="sat-18">xxxx</td>
             </tr>
             <tr>
                 <td>18h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1830">xxxx</td>
+                <td id="tue-1830">xxxx</td>
+                <td id="wed-1830">xxxx</td>
+                <td id="thu-1830">xxxx</td>
+                <td id="fri-1830">xxxx</td>
+                <td id="sat-1830">xxxx</td>
             </tr>
             <tr>
                 <td>19h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-19">xxxx</td>
+                <td id="tue-19">xxxx</td>
+                <td id="wed-19">xxxx</td>
+                <td id="thu-19">xxxx</td>
+                <td id="fri-19">xxxx</td>
+                <td id="sat-19">xxxx</td>
             </tr>
             <tr>
                 <td>19h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-1930">xxxx</td>
+                <td id="tue-1930">xxxx</td>
+                <td id="wed-1930">xxxx</td>
+                <td id="thu-1930">xxxx</td>
+                <td id="fri-1930">xxxx</td>
+                <td id="sat-1930">xxxx</td>
             </tr>
             <tr>
                 <td>20h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-20">xxxx</td>
+                <td id="tue-20">xxxx</td>
+                <td id="wed-20">xxxx</td>
+                <td id="thu-20">xxxx</td>
+                <td id="fri-20">xxxx</td>
+                <td id="sat-20">xxxx</td>
             </tr>
             <tr>
                 <td>20h30</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-2030">xxxx</td>
+                <td id="tue-2030">xxxx</td>
+                <td id="wed-2030">xxxx</td>
+                <td id="thu-2030">xxxx</td>
+                <td id="fri-2030">xxxx</td>
+                <td id="sat-2030">xxxx</td>
             </tr>
             <tr>
                 <td>21h</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
-                <td>xxxx</td>
+                <td id="mon-21">xxxx</td>
+                <td id="tue-21">xxxx</td>
+                <td id="wed-21">xxxx</td>
+                <td id="thu-21">xxxx</td>
+                <td id="fri-21">xxxx</td>
+                <td id="sat-21">xxxx</td>
             </tr>
         </table>
-        <p id="machine">test</p>
         <script src="js/parser.js"></script>
     </main>
 </body>
--- src/Website/js/parser.js
+++ src/Website/js/parser.js
@@ -2,6 +2,32 @@
 let handleFile = (json) => {
     if (!json || typeof(json) != "object") throw new Error("No JSON data provided"); // dans le cas hypotétique où il y aurai pas d'objet en réponse
 
+    for (let i  = 0; i < json.monday.length; i++)
+    {
+        let startCourse = {
+            hour: new Date(json.monday[i].hour.start).getHours() - 2,
+            minute: new Date(json.monday[i].hour.start).getMinutes()
+        }
+
+        let stopCourse = {
+            hour: new Date(json.monday[i].hour.end).getHours() - 2,
+            minute: new Date(json.monday[i].hour.end).getMinutes()
+        }
+
+        let courseLength = stopCourse.hour - startCourse.hour
+
+        let idstart;
+        
+        console.log(startCourse.minute)
+        if (startCourse.minute == 0)
+            idstart = `${startCourse.hour}`
+        else
+            idstart = `${startCourse.hour}${startCourse.minute}`;
+        
+        console.log("mon-" + idstart)
+        updateHTML("#mon-" + idstart, json.monday[i].name)
+        console.log("durée " + courseLength);
+    }
     
 
 }
@@ -13,10 +39,11 @@ let updateHTML = (CSSSelector, newVal) => {
     tag.innerHTML = newVal;
 }
 
-updateHTML("#machine", "toast")
-
 // on charge le fichier JSON dans le JS lors du chargmeent
-const URL_OF_THE_FILE = "http://localhost/SimpleScheduleViewer/schedule.json";
+const URL_OF_THE_FILE = {
+    DEV: "http://localhost/SimpleScheduleViewer/schedule.json",
+    PROD: "xxx"
+}
 let scheduleFile = new XMLHttpRequest()
 
 // fonction appelée à chaque changement d'état de la requête
@@ -27,7 +54,7 @@ scheduleFile.onreadystatechange = () => {
 }
 
 scheduleFile.responseType = "application/json";
-scheduleFile.open("GET", URL_OF_THE_FILE, false);
+scheduleFile.open("GET", URL_OF_THE_FILE.DEV, false);
 
 // on try/catch l'envoi de la requête
 try {
