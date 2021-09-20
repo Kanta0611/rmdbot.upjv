cfad912b0c861494e3c98e34d55fa6686dad9169
yama <42041037+oyamabs@users.noreply.github.com>
2021-09-20 11:25:21 +0200

    Made API (not secure yet)

--- src/Website/api.php
+++ src/Website/api.php
@@ -1,7 +1,152 @@
 <?php
 
-if (!isset($_GET["api"])) {
-	echo "Please provide api type";
+
+/**
+ * @param string $courseDay Jour du cours (lundi, mardi, mercredi, jeudi, vendredi, samedi)
+ * @param mixed $courseName Nom du cours
+ * @param mixed $courseRoom Salle du cours
+ * @param mixed $courseStart Heure (format ISO) du cours
+ * @param mixed $courseLength Durée du cours (en n-demi-heures)
+ * @param mixed $courseTeacher Prof du cours
+ * 
+ * @return void
+ */
+function addObject($courseDay, $courseName, $courseRoom, $courseStart, $courseLength, $courseTeacher) {
+    $filecontent = file_get_contents("schedule.json"); // charge le contenu du fichier
+    $data = json_decode($filecontent, true);
+	$guid = uniqid();
+
+    $addedobj = [
+        "uuid" => $guid,
+        "name" => $courseName,
+        "room" => $courseRoom,
+        "hour" => [
+            "start" => $courseStart,
+            "length" => $courseLength
+        ],
+        "teacher" => $courseTeacher
+    ];
+
+    $id = sizeof($data[$courseDay]);
+    $data[$courseDay][$id] = $addedobj;
+
+    file_put_contents("schedule.json", json_encode($data));
+}
+
+/**
+ * Fonction supprimant un cours au fichiers JSON en founissant son ID
+ */
+function removeObject($guid) {
+    
+    $filecontent = file_get_contents("schedule.json"); // charge le contenu du fichier
+    $data = json_decode($filecontent, true);
+
+    for($i = 0; $i < sizeof($data['monday']); $i++) {
+        if ($guid == $data['monday'][$i]['uuid']) {
+           unset($data['monday'][$i]);
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['tuesday']); $i++) {
+        if ($guid == $data['tuesday'][$i]['uuid']) {
+           unset($data['tuesday'][$i]);
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['wednesday']); $i++) {
+        if ($guid == $data['wednesday'][$i]['uuid']) {
+           unset($data['wednesday'][$i]);
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['thursday']); $i++) {
+        if ($guid == $data['thursday'][$i]['uuid']) {
+           unset($data['thursday'][$i]);
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['friday']); $i++) {
+        if ($guid == $data['friday'][$i]['uuid']) {
+           unset($data['friday'][$i]);
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['saturday']); $i++) {
+        if ($guid == $data['saturday'][$i]['uuid']) {
+           unset($data['saturday'][$i]);
+        }
+    }
+
+    file_put_contents("schedule.json", json_encode($data));
+}
+
+
+function editObject($guid, $courseName, $courseRoom, $courseStart, $courseLength, $courseTeacher) {
+    
+    $filecontent = file_get_contents("schedule.json"); // charge le contenu du fichier
+    $data = json_decode($filecontent, true);
+
+    $editeddobj = [
+        "uuid" => $guid,
+        "name" => $courseName,
+        "room" => $courseRoom,
+        "hour" => [
+            "start" => $courseStart,
+            "length" => $courseLength
+        ],
+        "teacher" => $courseTeacher
+    ];
+
+    for($i = 0; $i < sizeof($data['monday']); $i++) {
+        if ($guid == $data['monday'][$i]['uuid']) {
+            $data['monday'][$i] = $editeddobj;
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['tuesday']); $i++) {
+        if ($guid == $data['tuesday'][$i]['uuid']) {
+            $data['tuesday'][$i] = $editeddobj;
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['wednesday']); $i++) {
+        if ($guid == $data['wednesday'][$i]['uuid']) {
+            $data['wednesday'][$i] = $editeddobj;
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['thursday']); $i++) {
+        if ($guid == $data['thursday'][$i]['uuid']) {
+            $data['thursday'][$i] = $editeddobj;
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['friday']); $i++) {
+        if ($guid == $data['friday'][$i]['uuid']) {
+            $data['friday'][$i] = $editeddobj;
+        }
+    }
+
+    for($i = 0; $i < sizeof($data['saturday']); $i++) {
+        if ($guid == $data['saturday'][$i]['uuid']) {
+            $data['saturday'][$i] = $editeddobj;
+        }
+    }
+
+    file_put_contents("schedule.json", json_encode($data));
+}
+
+?>
+
+<?php
+
+if (!isset($_GET["api"])) {$data = [
+    "message" => "No API Endpoint provided or endpoint is not valid"
+];
+
+header("Content-type: application/json");
+echo(json_encode($data));
+
 	return;
 }
 
@@ -9,6 +154,83 @@ if (!isset($_GET["api"])) {
 	// getVersion
 switch ($_GET["api"]) {
 	case "getVersion":
-		echo "a1.0";
+		echo "1.0";
 		break;
+    
+    case "addCourse":
+        if (isset($_GET['day']) && $_GET['day'] != "" && isset($_GET['name']) && $_GET['name'] != ""  && isset($_GET['room']) && $_GET['room'] != "" && isset($_GET['start']) && $_GET['start'] != "" && isset($_GET['length']) && $_GET['length'] != 0 && isset($_GET['teacher']) && $_GET['teacher'] != "") {
+            addObject($_GET['day'], $_GET['name'], $_GET['room'], $_GET['start'], $_GET['length'], $_GET['teacher']);
+            $state =  [
+                "state" => "OK"
+            ];
+            header("Content-type: application/json");
+            echo(json_encode($state));
+        } else {
+            $state =  [
+                "state" => "There are missing arguments"
+            ];
+            header("Content-type: application/json");
+            echo(json_encode($state));      
+        }
+        break;
+
+    case "removeCourse":
+        if (isset($_GET['guid']) && $_GET['guid'] != "") {
+            removeObject($_GET['guid']);
+
+            $state =  [
+                "state" => "OK"
+            ];
+            header("Content-type: application/json");
+            echo(json_encode($state));
+        
+        } else {
+            $state =  [
+                "state" => "Missing GUID"
+            ];
+            header("Content-type: application/json");
+            echo(json_encode($state));
+        }
+        break;
+    
+    case "clearWeek":
+        $filecontent = file_get_contents("schedule.json"); // charge le contenu du fichier
+        $data = [
+            "monday" => [],
+            "tuesday" => [],
+            "wednesday" => [],
+            "thursday" => [],
+            "friday" => [],
+            "saturday" => []
+        ];
+
+        file_put_contents("schedule.json", json_encode($data));
+        break;
+
+    case "editCourse":
+        if (isset($_GET['guid']) && $_GET['guid'] && isset($_GET['name']) && $_GET['name'] != ""  && isset($_GET['room']) && $_GET['room'] != "" && isset($_GET['start']) && $_GET['start'] != "" && isset($_GET['length']) && $_GET['length'] != 0 && isset($_GET['teacher']) && $_GET['teacher'] != "") {
+            editObject($_GET['guid'], $_GET['name'], $_GET['room'], $_GET['start'], $_GET['length'], $_GET['teacher']);
+            $state =  [
+                "state" => "OK"
+            ];
+            header("Content-type: application/json");
+            echo(json_encode($state));
+        } else {
+            $state =  [
+                "state" => "There are missing arguments"
+            ];
+            header("Content-type: application/json");
+            echo(json_encode($state));      
+        }
+        break;
+    
+    default:
+        $data = [
+            "message" => "No API Endpoint provided or endpoint is not valid"
+        ];
+
+        header("Content-type: application/json");
+        echo(json_encode($data));
 }
+
+?>
